99727e7e636d561d9ec94ed7e7a5f28393be9aec
Maciej Kutrowski <maciej.michal.kutrowski@gmail.com>
2021-11-05 16:38:49 +0100

    final

--- build.sbt
+++ build.sbt
@@ -1,12 +1,6 @@
 name := "Json_downloader"
-
 version := "0.1"
-
 scalaVersion := "2.13.7"
-//libraryDependencies += "com.typesafe.akka" %% "akka-http-spray-json" % AkkaHttpVersion
-
-//libraryDependencies +=
-//  "com.typesafe.akka" %% "akka-http-spray-json" % "10.2.6"
 
 libraryDependencies +=
   "com.typesafe.akka" %% "akka-actor" % "2.6.17"
@@ -14,9 +8,9 @@ libraryDependencies +=
 libraryDependencies +=
   "com.typesafe.akka" %% "akka-http-core" % "10.2.6"
 libraryDependencies += "com.lihaoyi" %% "upickle" % "1.4.2"
-val AkkaVersion = "2.6.17"
-libraryDependencies ++= Seq(
-  "com.typesafe.akka" %% "akka-stream" % AkkaVersion)
 
-val AkkaHttpVersion = "10.2.7"
-libraryDependencies += "com.typesafe.akka" %% "akka-http-spray-json" % AkkaHttpVersion
+libraryDependencies ++= Seq(
+  "com.typesafe.akka" %% "akka-stream" % "2.6.17")
+
+libraryDependencies += "com.typesafe.akka" %% "akka-http-spray-json" % "10.2.7"
+libraryDependencies += "com.typesafe" % "config" % "1.4.1"
--- src/main/scala/Downloader.scala
+++ src/main/scala/Downloader.scala
@@ -1,19 +1,14 @@
-
 import akka.actor.ActorSystem
 import akka.http.scaladsl.unmarshalling.Unmarshal
-import spray.json.JsValue
 import spray.json._
-
 import java.io.{File, PrintWriter}
 import scala.concurrent.duration.DurationInt
 import scala.concurrent.{Await, Future}
 import akka.http.scaladsl.Http
-
 import scala.concurrent.ExecutionContext.Implicits.global
 import akka.http.scaladsl.model.{HttpRequest, HttpResponse}
 import spray.json.DefaultJsonProtocol
-
-import scala.util.{Failure, Success, Try}
+import com.typesafe.config._
 
 case class Post(body: String, id: Int, title: String, userId: Int)  {
   def savePost(path: String): Unit = {
@@ -30,33 +25,21 @@ object PostJsonProtocol extends DefaultJsonProtocol {
   implicit val postFormat: RootJsonFormat[Post] = jsonFormat4(Post.apply)
 }
 
+object Downloader extends App {
+  import PostJsonProtocol._
+  import spray.json._
+  val conf = ConfigFactory.parseFile(new File("resources/application.conf"))
+  val system: ActorSystem = ActorSystem("post-download")
+  implicit val sys: ActorSystem = system
+  val responseFuture: Future[HttpResponse] =
+  Http().singleRequest(HttpRequest(uri = conf.getString("download-uri")))
 
-object Downloader {
-  def main(args: Array[String]): Unit = {
-    val system: ActorSystem = ActorSystem("test")
-    implicit val sys: ActorSystem = system
-    val responseFuture: Future[HttpResponse] =
-      Http().singleRequest(HttpRequest(uri = "https://jsonplaceholder.typicode.com/posts"))
+  val jsonStringFuture = responseFuture.flatMap{ r: HttpResponse => Unmarshal(r).to[String]}
+  val jsonValFut: Future[JsValue] = jsonStringFuture.map(_.parseJson)
+  val jsonVal: JsValue = Await.result(jsonValFut, conf.getInt("download-timeout-sec").seconds)
+  val json = jsonVal.toJson
+  val listOfJsObjects = json.convertTo[List[JsObject]]
+  val listOfPosts = listOfJsObjects.map(_.convertTo[Post])
 
-    val jsonStringFuture = responseFuture.flatMap{ r: HttpResponse => Unmarshal(r).to[String]}
-    val jsonValFut: Future[JsValue] = jsonStringFuture.map(_.parseJson)
-
-    import spray.json._
-//    val jsonToParse = jsonValFut.map(_.convertTo[List[JsObject]])
-    val jsonVal: JsValue = Await.result(jsonValFut, 5.seconds)
-    println(jsonVal.prettyPrint)
-    import PostJsonProtocol._
-
-    val listOfPosts =
-    Try {
-      val json = jsonVal.toJson
-      val listOfJsObjects = json.convertTo[List[JsObject]]
-      listOfJsObjects.map(_.convertTo[Post])
-    } match {
-      case Failure(e: DeserializationException) => throw e
-//      case Failure(e: DeserializationException) => throw e
-      case Success(value) => value
-    }
-    listOfPosts.foreach(_.savePost("./resources"))
-    }
+  listOfPosts.foreach(_.savePost(conf.getString("download-folder")))
 }
